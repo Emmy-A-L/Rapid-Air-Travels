3f25824d5ffd343f3847a5c416f532f0cf9ed6a8
Emmy-A-L <elot833@gmail.com>
2025-04-02 17:18:14 +0100

    initialize React application with Vite setup and basic structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+
+  return (
+    <div>
+      <h1 style={{color:"blue", fontSize: '6rem'} }>Welcome to the App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+
+}
+
+export default App;
